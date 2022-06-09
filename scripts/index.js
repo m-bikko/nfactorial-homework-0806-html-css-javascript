951b2caa42838ba0501c75fff371fba79cb1713b
beimbet.maratov <beimbet.maratov@mail.ru>
2022-06-09 14:53:24 +0600

    The homework 3 silver

---
+++ scripts/index.js
@@ -0,0 +1,69 @@
+        // const background = document.getElementById("video-background")
+        // const container = document.getElementById("container")
+        // let playVideoBtn = document.getElementById("btn-change-background")
+        // const playPauseTrackBtn = document.getElementById("playpause-track")
+        // const audio = document.getElementById("audio")
+        // const progressBar = document.getElementById("progressBar")
+        //  playPauseTrackBtn.addEventListener("click", playPauseTrack());
+        // function playPause() {
+        //   if (isPlaying) {
+        //     audio.pause();
+        //   } else {
+        //     audio.play();
+        //   }
+        //   isPlaying = !isPlaying;
+        // } 
+        // function playVideo(){
+        //   if(background.paused){
+        //     background.play();
+        //     playVideoBtn.style.backgroundColor = 'black'
+        //   }else{
+        //     background.pause()
+        //     playVideoBtn.style.backgroundColor = 'transparent'
+        //     // playVideoBtn.innerHTML = '<p style="color: white;">Party Mode</p>'
+        //   }
+        // }
+
+        // function playPauseTrack(){
+        //   if(audio.paused){
+        //     audio.play();
+        //     playPauseTrackBtn.innerHTML = '<i class="fa fa-pause-circle fa-3x"></i>';
+        //   }else{
+        //     audio.pause();
+        //     playPauseTrackBtn.innerHTML = '<i class="fa fa-play-circle fa-3x"></i>';
+
+        //   }
+        // }
+
+
+        // function progressValue() {
+        //   progressBar.max = track.duration;
+        //   progressBar.value = track.currentTime;
+
+        //   currentTime.textContent = formatTime(track.currentTime);
+        //   durationTime.textContent = formatTime(track.duration);
+        // }
+
+        // setInterval(progressValue, 500);
+
+        // function formatTime(sec) {
+        //   let minutes = Math.floor(sec / 60);
+        //   let seconds = Math.floor(sec - minutes * 60);
+        //   if (seconds < 10) {
+        //     seconds = `0${seconds}`;
+        //   }
+        //   return `${minutes}:${seconds}`;
+        // }
+
+        // function changeProgressBar() {
+        //   track.currentTime = progressBar.value;
+        // }
+
+        // progressBar.addEventListener("click", changeProgressBar);
+
+        // document.addEventListener('keyup', (event) => {
+        //   var name = event.key;
+        //   var code = event.code;
+        //   // Alert the key name and key code on keydown
+        //   alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+        // }, false);
